848bf906c1c4a78d6618a7f48379776767ac936b
Dmytro Brovchuk <70040636+debug-sys@users.noreply.github.com>
2024-01-25 19:13:39 +0100

    Version 3.6.2 - If the certificate string already has a prefix, there is no need to add it (#11)
    * New minor update 3.6.2: Fix - check certificate prefix

--- src/Util/Helper.php
+++ src/Util/Helper.php
@@ -70,6 +70,10 @@ final class Helper
 
     public static function formatPEM(string $certificate): string
     {
+        if (strpos($certificate, "-----BEGIN CERTIFICATE-----\n") !== false) {
+            return $certificate;
+        }
+
         return join("\n", [
             "-----BEGIN CERTIFICATE-----",
             $certificate,
