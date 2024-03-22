5192ddc407d7d173660b8710d79084e0801f9baf
xcsoft <42080379+soxft@users.noreply.github.com>
2024-03-22 23:40:12 +0800

    Feta: Enhanced HTTPRequestFailed Exception

--- src/Exception/HTTPRequestFailed.php
+++ src/Exception/HTTPRequestFailed.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Readdle\AppStoreServerAPI\Exception;
@@ -12,5 +13,25 @@ final class HTTPRequestFailed extends AppStoreServerAPIException
         } else {
             parent::__construct("HTTP request [$method $url] failed with status code $code. Response text is: $message", $code);
         }
+    }
+
+    /**
+     * Get the HTTP status code
+     *
+     * @return integer
+     */
+    public function getStatusCode(): int
+    {
+        return $this->getCode();
+    }
+
+    /**
+     * Get the response text
+     *
+     * @return string
+     */
+    public function getResponseText(): string
+    {
+        return $this->getMessage();
     }
 }
