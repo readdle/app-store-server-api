d7b336ef0b662fa7eb92338f7639b8e5f6e1fdbd
xcsoft <42080379+soxft@users.noreply.github.com>
2024-03-22 23:48:43 +0800

    Feta: add HTTPRequestFailed::getResponseText method

--- src/Exception/HTTPRequestFailed.php
+++ src/Exception/HTTPRequestFailed.php
@@ -1,16 +1,31 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Readdle\AppStoreServerAPI\Exception;
 
 final class HTTPRequestFailed extends AppStoreServerAPIException
 {
+    public string $responseText;
+
     public function __construct(string $method, string $url, string $message, int $code = 0)
     {
+        $this->responseText = $message;
+
         if ($code === 0) {
             parent::__construct("HTTP request [$method $url] failed: $message");
         } else {
             parent::__construct("HTTP request [$method $url] failed with status code $code. Response text is: $message", $code);
         }
+    }
+
+    /**
+     * Get the response text
+     *
+     * @return string
+     */
+    public function getResponseText(): string
+    {
+        return $this->responseText;
     }
 }
